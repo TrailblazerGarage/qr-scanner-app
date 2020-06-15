d135e8757001b01781f32f3196dd0d8c15530450
Jhony Vidal <joliveira@sms-latam.com>
2020-06-15 18:42:48 -0300

    Add BottomFloat to change Map Style

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -3,7 +3,17 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:qr_scanner_app/src/models/scan_model.dart';
 
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  final map = new MapController();
+  String typeStyleMap = 'streets-v11';
+
   @override
   Widget build(BuildContext context) {
 
@@ -15,16 +25,37 @@ class MapPage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location),
-            onPressed: (){},
+            onPressed: (){
+              map.move(scan.getLatLng(), 15);
+            },
           )
         ]
       ),
-      body: _createFlutterMap(scan)
+      body: _createFlutterMap(scan),
+      floatingActionButton: _createBottomFloat( context ),
+    );
+  }
+
+  Widget _createBottomFloat( BuildContext context) {
+    /// Types of styles provided by MapBox
+    /// https://docs.mapbox.com/api/maps/#styles
+    /// streets-v11,outdoors-v11,light-v10,dark-v10,satellite-streets-v11,satellite-v9
+    return FloatingActionButton(
+      child: Icon( Icons.repeat ),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        /// TODO the style of current portion of map is not changed onPressed only the parts around it
+        typeStyleMap = (typeStyleMap == 'streets-v11' )
+                        ? typeStyleMap = 'dark-v10'
+                        : typeStyleMap = 'streets-v11';
+        setState((){});
+      },
     );
   }
 
   Widget _createFlutterMap( ScanModel scan ) {
     return new FlutterMap(
+      mapController: map,
       options: new MapOptions(
         center: scan.getLatLng(),
         zoom: 15
@@ -38,12 +69,11 @@ class MapPage extends StatelessWidget {
 
   _createMap() {
     return TileLayerOptions(
-        urlTemplate: 'https://api.mapbox.com/styles/v1/mapbox/{id}/tiles'
-            '/{z}/{x}/{y}?access_token={accessToken}',
+        urlTemplate: 'https://api.mapbox.com/styles/v1/mapbox/'
+            '{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
         additionalOptions: {
           'accessToken':'pk.eyJ1IjoidHJhaWxibGF6ZXJzLWpob255dmlkYWwiLCJhIjoiY2tiZ3NqYzA3MThtZTJvbXVpdjA5NXVpbCJ9.AJuhjHqVqF49bO6JAqEpfg',
-          'id': 'streets-v11'
-          // streets, dark,light, outdoors, satellite
+          'id': '$typeStyleMap'
         }
     );
   }
